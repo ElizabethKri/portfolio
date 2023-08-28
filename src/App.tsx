ec08c8feb172bcdac1af18307e48988503e77b84
Elizaveta Kritskaya <post.kri@gmail.com>
2023-08-28 19:37:51 +0300

    add contact

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import {Main} from "./layout/sections/main/Main";
 import {Skills} from "./layout/sections/skills/Skills"
 import {Works} from "./layout/sections/works/Works";
 import {About} from "./layout/sections/about/About";
+import {Contact} from "./layout/sections/contact/Contact";
 
 
 
@@ -16,6 +17,7 @@ function App() {
             <Skills/>
             <Works/>
             <About/>
+            <Contact/>
         </div>
     );
 }
