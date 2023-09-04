00efa5f1791a65a48f95ed06743390d08bae47f2
Elizaveta Kritskaya <post.kri@gmail.com>
2023-09-04 20:38:31 +0300

    add container + FlexWrapper

--- src/layout/header/Header.tsx
+++ src/layout/header/Header.tsx
@@ -1,20 +1,24 @@
 import styled from "styled-components";
 import Logo from "../../components/logo/Logo";
 import Menu from "../../components/menu/Menu";
+import {Container} from "../../components/Container";
+import FlexWrapper from "../../components/FlexWrapper";
 
 const items = ["Home", "Skills", "Works", "About", "Contact"]
 
 export const Header = () => {
     return (
         <StyledHeader>
-            <Logo/>
-            <Menu menuItems={items}/>
+            <Container>
+                <FlexWrapper justify={"space-between"} align={"center"}>
+                    <Logo/>
+                    <Menu menuItems={items}/>
+                </FlexWrapper>
+            </Container>
         </StyledHeader>
     );
 };
 
 const StyledHeader = styled.header`
   background-color: #fde8fd;
-  display: flex;
-  justify-content: space-between;
 `
