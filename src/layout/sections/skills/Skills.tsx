e639868d9fd3d0774e0a2929a9f6e5f51e6a439f
Elizaveta Kritskaya <post.kri@gmail.com>
2023-09-04 21:37:58 +0300

    add skills - styled components

--- src/layout/sections/skills/Skills.tsx
+++ src/layout/sections/skills/Skills.tsx
@@ -3,32 +3,30 @@ import styled from "styled-components";
 import FlexWrapper from "../../../components/FlexWrapper";
 import {SectionTitle} from "../../../components/SectionTitle";
 import {Skill} from "./skill/Skill";
+import {Container} from "../../../components/Container";
 
 export const Skills = () => {
     return (
         <StyledSkills>
-            <SectionTitle>My Skills</SectionTitle>
-            <FlexWrapper wrap={"wrap"} justify ={"space-between"}>
-                <Skill iconId={"html"} title={"html5"}/>
-                <Skill iconId={"css"} title={"css3"}/>
-                <Skill iconId={"react"} title={"react"}/>
-                <Skill iconId={"angular"} title={"angular"}/>
-                <Skill iconId={"bootstrap"} title={"bootstrap"}/>
-                <Skill iconId={"nest"} title={"nestJs"}/>
-                <Skill iconId={"js"} title={"js"}/>
-                <Skill iconId={"figma"} title={"figma"}/>
-                <Skill iconId={"github"} title={"github"}/>
-                <Skill iconId={"mongoDb"} title={"mongoDb"}/>
-                <Skill iconId={"git"} title={"git"}/>
-                <Skill iconId={"styledComponent"} title={"styled component"}/>
-                <Skill iconId={"typescript"} title={"typescript"}/>
-
-            </FlexWrapper>
-
+            <Container><SectionTitle>My Skills</SectionTitle>
+                <FlexWrapper wrap={"wrap"} justify ={"space-between"}>
+                    <Skill iconId={"html"} title={"html5"}/>
+                    <Skill iconId={"css"} title={"css3"}/>
+                    <Skill iconId={"react"} title={"react"}/>
+                    <Skill iconId={"angular"} title={"angular"}/>
+                    <Skill iconId={"bootstrap"} title={"bootstrap"}/>
+                    <Skill iconId={"nest"} title={"nestJs"}/>
+                    <Skill iconId={"js"} title={"js"}/>
+                    <Skill iconId={"figma"} title={"figma"}/>
+                    <Skill iconId={"github"} title={"github"}/>
+                    <Skill iconId={"mongoDb"} title={"mongoDb"}/>
+                    <Skill iconId={"git"} title={"git"}/>
+                    <Skill iconId={"styledComponent"} title={"styled component"}/>
+                    <Skill iconId={"typescript"} title={"typescript"}/>
+                </FlexWrapper>
+            </Container>
         </StyledSkills>
     );
 };
 
-const StyledSkills = styled.section `
-  background-color: #fde8fd;
-`
+const StyledSkills = styled.section ``
--- src/layout/sections/skills/skill/Skill.tsx
+++ src/layout/sections/skills/skill/Skill.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Icon from "../../../../components/icon/Icon";
 import styled from "styled-components";
+import FlexWrapper from "../../../../components/FlexWrapper";
 
 type SkillPropsType = {
     iconId: string;
@@ -10,17 +11,47 @@ type SkillPropsType = {
 export const Skill = (props: SkillPropsType) => {
     return (
         <StyledSkill>
-            <Icon iconId={props.iconId}/>
-            <SkillTitle>{props.title}</SkillTitle>
+            <FlexWrapper direction={"column"} align={"center"}>
+                <IconWrapper>
+                    <Icon iconId={props.iconId}/>
+                </IconWrapper>
+
+                <SkillTitle>{props.title}</SkillTitle>
+            </FlexWrapper>
         </StyledSkill>
     );
 };
 
 const StyledSkill = styled.div`
-  margin-left: 20px;
+  width: 380px;
+  padding: 10px;
 
 `
 const SkillTitle = styled.h3`
+  margin-top: 25px;
+  font-family: 'Josef Sans', sans-serif;
+  font-weight: 700;
+  font-size: 16px;
+  letter-spacing: 1px;
+  text-transform: uppercase;
+`
+
+const IconWrapper = styled.div`
+  position: relative;
+
+  &::before {
+    content: '';
+    display: inline-block;
+    width: 65px;
+    height: 65px;
+    background-color: rgba(191, 5, 243, 0.1);
+    transform: rotate(45deg) translate(-50%, -50%);
+    
+    position: absolute;
+    left: 50%;
+    top: 50%;
+    transform-origin: top left;
+  }
 
 `
 
