9d902ea6f0d369f1744ff3e6a42de45b4622fd8b
Elizaveta Kritskaya <post.kri@gmail.com>
2023-09-10 15:29:47 +0300

    add footer - refactoring

--- src/layout/footer/Footer.tsx
+++ src/layout/footer/Footer.tsx
@@ -1,86 +1,40 @@
 import React from 'react';
-import styled from "styled-components";
 import Icon from "../../components/icon/Icon";
 import FlexWrapper from "../../components/FlexWrapper";
-import {theme} from "../../styles/Theme";
-import {font} from "../../styles/Common";
+import {S} from "./Footer_Styles"
+
+
+const socialItemsData = [
+    {
+        iconId: "vk",
+    },
+    {
+        iconId: "instagram",
+    },
+    {
+        iconId: "telegram",
+    },
+]
 
 export const Footer = () => {
     return (
-        <StyledFooter>
+        <S.Footer>
             <FlexWrapper direction={"column"} align={"center"}>
-                <Name>Elizabeth</Name>
-                <SocialList>
-                    <SocialItem>
-                        <SocialLink>
-                            <Icon height={"21px"} width={"21px"} viewBox={"0 0 21px 21px"} iconId={"vk"}/>
-                        </SocialLink>
-                    </SocialItem>
-                    <SocialItem>
-                        <SocialLink>
-                            <Icon height={"21px"} width={"21px"} viewBox={"0 0 21px 21px"} iconId={"instagram"}/>
-                        </SocialLink>
-                    </SocialItem>
-                    <SocialItem>
-                        <SocialLink>
-                            <Icon height={"21px"} width={"21px"} viewBox={"0 0 21px 21px"} iconId={"telegram"}/>
-                        </SocialLink>
-                    </SocialItem>
-                </SocialList>
-                <Copyright>@ 2023 Elizabeth Kritskaya, All Rights Reserved</Copyright>
+                <S.Name>Elizabeth</S.Name>
+                <S.SocialList>
+
+                    {socialItemsData.map((ic, index) => {
+                        return <S.SocialItem key={index}>
+                            <S.SocialLink>
+                                <Icon height={"21px"} width={"21px"} viewBox={"0 0 21px 21px"} iconId={ic.iconId}/>
+                            </S.SocialLink>
+                        </S.SocialItem>
+                    })}
+
+                </S.SocialList>
+                <S.Copyright>@ 2023 Elizabeth Kritskaya, All Rights Reserved</S.Copyright>
             </FlexWrapper>
-        </StyledFooter>
+        </S.Footer>
     );
 };
 
-const StyledFooter = styled.footer`
-  background-color: ${theme.colors.headerFooter};
-  padding: 40px;
-
-`
-
-const SocialList = styled.ul`
-  display: flex;
-  gap: 20px;
-
-  margin: 30px 0;
-
-
-`
-
-const SocialItem = styled.li`
-
-`
-
-const SocialLink = styled.a`
-  background-color: ${theme.colors.primaryBg};
-  opacity: 0.7;
-  border-radius: 50%;
-  width: 35px;
-  height: 35px;
-  
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  
-  color: ${theme.colors.accent};
-  
-  &:hover{
-    color: ${theme.colors.font};
-    transform: translateY(-4px);
-  }
-`
-
-const Name = styled.span`
-  
-  ${font({family: "'Josefin Sans', sans-serif", weight: 700, Fmax: 22, Fmin: 16})}
-  letter-spacing: 3px; 
-  
-`
-
-const Copyright = styled.small`
-  font-weight: 400;
-  font-size: 12px;
-  text-align: center;
-  opacity: 0.5;
-`
