f1f80a7d091c011923d3b94d0abfee1a8aca87b0
Elizaveta Kritskaya <post.kri@gmail.com>
2023-09-10 14:59:56 +0300

    add works - refactoring

--- src/layout/sections/works/Works.tsx
+++ src/layout/sections/works/Works.tsx
@@ -1,39 +1,48 @@
 import React from 'react';
-import styled from "styled-components";
 import {SectionTitle} from "../../../components/SectionTitle";
-
 import FlexWrapper from "../../../components/FlexWrapper";
 import {Work} from "./work/Work";
 import socialImg from './../../../assets/images/project1.webp'
 import timerImg from './../../../assets/images/project3.webp'
 import {TabMenu} from "./tabMenu/TabMenu";
 import {Container} from "../../../components/Container";
+import {S} from "./Works_Styles"
 
 const workItems = ["All", "Landing page", "React", "Angular"]
 
- export const Works = () => {
+const workData = [
+    {
+        title:"Online store of branded bags",
+        src: socialImg,
+        text: "\"Starbrands\" is an Italian premium brand that\n" +
+            "produces leather goods. The company produces bags of the highest class\n" +
+            "men's and women's collections."
+    },
+    {
+        title:"Timer",
+        src: timerImg,
+        text: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua Ut enim. Lorem ipsum dolor sit amet, consectetur adipisicing elit ut labore et dolore magna aliqua Ut enim"
+    },
+]
+
+ export const Works: React.FC = () => {
     return (
-        <StyledWorks>
+        <S.Works>
             <Container>
                 <SectionTitle>My Works</SectionTitle>
                 <TabMenu menuItems={workItems}/>
                 <FlexWrapper justify={"space-between"} align={"flex-start"} wrap={"wrap"}>
-                    <Work title={"Online store of branded bags"}
-                          text={"\"Starbrands\" is an Italian premium brand that\n" +
-                              "produces leather goods. The company produces bags of the highest class\n" +
-                              "men's and women's collections."}
-                          src={socialImg}/>
-                    <Work title={"Timer"}
-                          text={"Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua Ut enim. Lorem ipsum dolor sit amet, consectetur adipisicing elit ut labore et dolore magna aliqua Ut enim"}
-                          src={timerImg}/>
+                    {workData.map((w)=>
+                        {
+                            return <Work title={w.title}
+                                         text={w.text}
+                                         src={w.src}/>
+                        }
+                    )}
+
                 </FlexWrapper>
             </Container>
-        </StyledWorks>
+        </S.Works>
     );
 };
 
-const StyledWorks = styled.section `
-    ${FlexWrapper}{
-      gap: 30px;
-    }
-    `
