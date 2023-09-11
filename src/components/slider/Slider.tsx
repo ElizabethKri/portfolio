2bab651d5ea10a7ff56c32ff35cab91cab24652e
Elizaveta Kritskaya <post.kri@gmail.com>
2023-09-11 20:01:08 +0300

    change description

--- src/components/slider/Slider.tsx
+++ src/components/slider/Slider.tsx
@@ -17,9 +17,9 @@ const Slide = (props: SlidePropsType) =>{
 }
 
 const items = [
-    <Slide text ={"In 2020, I graduated from the St. Petersburg State University of Railways of Emperor Alexander First with a degree in Accounting, Analysis and Audit. After graduating from the university, I started career in the field of audit, which takes into account the attractiveness and accuracy to the verification of financial statements."}/>,
-    <Slide text ={"In 2022, I decided to try myself in a new direction as a frontend developer. I enrolled for retraining at Peter the Great St. Petersburg Polytechnic University, where she received basic concepts and further deepened knowledge in frameworks."}/>,
-    <Slide text ={"In fact, I am a versatile person, I like to study useful information for myself and plan my time. I also try to find time to spend my leisure time: I visit museums and exhibitions, wine and cheese tastings, pottery, etc."}/>
+    <Slide text ={"In 2020, I graduated from St. Petersburg State University of Transport of Emperor Alexander the First with a degree in Accounting, Analysis and Audit. After graduating from the university, I started career in the field of audit, which takes into account the attractiveness and accuracy to the verification of financial statements."}/>,
+    <Slide text ={"In 2022, I decided to pick a new direction in my career as a front-end developer. With this, I went for retraining at Peter the Great St. Petersburg Polytechnic University, where I acquired basic concepts and later developed knowledge in frameworks."}/>,
+    <Slide text ={"In fact, I am a versatile person, I like to get useful information for me and plan my time. I also try to find time to spend for various leisure activities: I visit museums and exhibitions, wine and cheese tastings, pottery, etc. "}/>
 ];
 
 export const Slider: React.FC = () => (
